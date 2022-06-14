7330498943f0b1d0a288b0f87e086feca2aba1ca
Sbkia <105112637+Sbkia@users.noreply.github.com>
2022-06-14 09:19:16 +0200

    Update helpers.py

--- sasdi_web/sasdi_web/helpers.py
+++ sasdi_web/sasdi_web/helpers.py
@@ -1,5 +1,3 @@
-from PIL import Image
-import os
 from os import walk
 from . import sasdi_functions as sf
 from pathlib import Path
@@ -7,6 +5,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 def get_client_ip(request):
+    """get the ip address of the user making the request"""
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
         ip = x_forwarded_for.split(',')[0]
@@ -15,11 +14,13 @@ def get_client_ip(request):
     return ip
 
 def get_aspect_ratio(image_path):
+    """returns the first frame by default"""
     im = Image.open(image_path)
     width, height = im.size
     return width / height
 
 def get_video_file_name(request):
+    """return list of files"""
     main_fullpath = os.path.join(BASE_DIR, 'videos', get_client_ip(request))
     _, _, filenames = next(walk(main_fullpath), (None, None, []))
     list_videos, _ = sf.save_get_list_videos(main_fullpath, filenames) 
@@ -28,4 +29,3 @@ def get_video_file_name(request):
         return list_videos[0][0]
     except:
         return ""
-
